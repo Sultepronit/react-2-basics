79854e4908206b266611dad609d156570824f28d
Sultepronit <sultepronit@gmail.com>
2023-06-23 14:19:51 +0300

    added some click events

--- src/Contents.js
+++ src/Contents.js
@@ -5,11 +5,28 @@ const Contents = () => {
         return Math.floor(Math.random() * 1000);
     }
 
+    function handleClick() {
+        console.log('Clicked!');
+    }
+
+    function handleClick2(parameter) {
+        console.log(`${parameter} inserted!`);
+    }
+
+    function handleClick3(e) {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);
+    }
+
     return (
     <main>
         <p>1 + 2</p>
         <p>{1 + 2}</p>
         <p>Here's your random number: {returnRandomInt()}</p>
+        <button onClick={handleClick}>Click me!</button>
+        <button onClick={() => handleClick2('Hello!')}>Click me too!</button>
+        <button onClick={(e) => handleClick3(e)}>And me!</button>
     </main>
     )
 }
