b040177abfede905f984df4bd0ca3715df7eabf3
Sultepronit <sultepronit@gmail.com>
2023-06-25 11:18:20 +0300

    added list with checkboxes

--- src/Contents.js
+++ src/Contents.js
@@ -1,49 +1,70 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 function Contents() {
-    const [number, setNumber] = useState(0);
-    const [timer, setTimer] = useState(0);
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: 'something'
+        },
+        {
+            id: 2,
+            checked: false,
+            item: 'something else'
+        },
+        {
+            id: 3,
+            checked: false,
+            item: 'item 3'
+        }
+    ]);
 
-    function setRandomInt() {
-        setNumber(Math.floor(Math.random() * 1000));
+    function handleCheck(id) {
+        console.log(`key: ${id}`);
+        const listItems = items.map(item => (item.id === id) ? { ...item, checked: !item.checked } : item);
+        console.log(listItems);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     }
 
-    // Doesn't work properly.
-    // Looks like there are several threads that make chaos.....
-    function handleTimer() {
-        setInterval(() => {
-            setTimer(timer + 2);
-            console.log(timer);
-        }, 2000);
-    }
-    //handleTimer();
-    //if(timer < 1) handleTimer();
-
-    function handleClick() {
-        console.log('Clicked!');
-    }
-
-    function handleClick2(parameter) {
-        console.log(`${parameter} inserted!`);
-    }
-
-    function handleClick3(e) {
-        console.log(e);
-        console.log(e.target);
-        console.log(e.target.innerText);
+    function handleDelete(id) {
+        const listItems = items.filter(item => item.id !== id);
+        console.log(listItems);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     }
 
     return (
-    <main>
-        <p>1 + 2</p>
-        <p>{1 + 2}</p>
-        <p>Timer: {timer}</p>
-        <p>Here's your random number: {number}</p>
-        <button onClick={setRandomInt}>change number!</button>
-        <button onClick={handleClick}>Click me!</button>
-        <button onClick={() => handleClick2('Hello!')}>Click me too!</button>
-        <button onClick={(e) => handleClick3(e)}>And me!</button>
-    </main>
+        <main>
+            {items.length ? (
+                <ul>
+                    {
+                        items.map(item => (
+                            <li className="item" key={item.id}>
+                                <input
+                                    type="checkbox"
+                                    onChange={() => handleCheck(item.id)}
+                                    checked={item.checked}
+                                />
+                                <label
+                                    style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                                    onDoubleClick={() => handleCheck(item.id)}
+                                >{item.item}</label>
+                                <FaTrashAlt
+                                    onClick={() => handleDelete(item.id)}
+                                    role="button"
+                                    tabIndex="0"
+                                />
+                            </li>
+                        ))
+                    }
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>List is empty!</p>
+            )}
+            
+        </main>
     )
 }
 
